61288cb2f5d1b9cea08edc2b7b4a73dee5869c5e
Frankie Dintino <fdintino@frankiemacbookpro.local>
2015-04-05 16:58:39 -0400

    Rewrite to use jQuery Sortable instead of jQuery UI's sortable

--- src/knockout-sortable.js
+++ src/knockout-sortable.js
@@ -17,13 +17,9 @@
         INDEXKEY = "ko_sourceIndex",
         LISTKEY = "ko_sortList",
         PARENTKEY = "ko_parentList",
-        DRAGKEY = "ko_dragItem",
         unwrap = ko.utils.unwrapObservable,
         dataGet = ko.utils.domData.get,
-        dataSet = ko.utils.domData.set,
-        version = $.ui && $.ui.version,
-        //1.8.24 included a fix for how events were triggered in nested sortables. indexOf checks will fail if version starts with that value (0 vs. -1)
-        hasNestedSortableFix = version && version.indexOf("1.6.") && version.indexOf("1.7.") && (version.indexOf("1.8.") || version === "1.8.24");
+        dataSet = ko.utils.domData.set;
 
     //internal afterRender that adds meta-data to children
     var addMetaDataAfterRender = function(elements, data) {
@@ -90,29 +86,6 @@
         return index;
     };
 
-    //remove problematic leading/trailing whitespace from templates
-    var stripTemplateWhitespace = function(element, name) {
-        var templateSource,
-            templateElement;
-
-        //process named templates
-        if (name) {
-            templateElement = document.getElementById(name);
-            if (templateElement) {
-                templateSource = new ko.templateSources.domElement(templateElement);
-                templateSource.text($.trim(templateSource.text()));
-            }
-        }
-        else {
-            //remove leading/trailing non-elements from anonymous templates
-            $(element).contents().each(function() {
-                if (this && this.nodeType !== 1) {
-                    element.removeChild(this);
-                }
-            });
-        }
-    };
-
     //connect items with observableArrays
     ko.bindingHandlers.sortable = {
         init: function(element, valueAccessor, allBindingsAccessor, data, context) {
@@ -120,9 +93,7 @@
                 value = unwrap(valueAccessor()) || {},
                 templateOptions = prepareTemplateOptions(valueAccessor, "foreach"),
                 sortable = {},
-                startActual, updateActual;
-
-            stripTemplateWhitespace(element, templateOptions.name);
+                onDragStartActual, onDropActual;
 
             //build a new object that has the global options with overrides from the binding
             $.extend(true, sortable, ko.bindingHandlers.sortable);
@@ -132,71 +103,43 @@
             }
             ko.utils.extend(sortable, value);
 
-            //if allowDrop is an observable or a function, then execute it in a computed observable
-            if (sortable.connectClass && (ko.isObservable(sortable.allowDrop) || typeof sortable.allowDrop == "function")) {
-                ko.computed({
-                    read: function() {
-                        var value = unwrap(sortable.allowDrop),
-                            shouldAdd = typeof value == "function" ? value.call(this, templateOptions.foreach) : value;
-                        ko.utils.toggleDomNodeCssClass(element, sortable.connectClass, shouldAdd);
-                    },
-                    disposeWhenNodeIsRemoved: element
-                }, this);
-            } else {
-                ko.utils.toggleDomNodeCssClass(element, sortable.connectClass, sortable.allowDrop);
-            }
-
             //wrap the template binding
             ko.bindingHandlers.template.init(element, function() { return templateOptions; }, allBindingsAccessor, data, context);
 
-            //keep a reference to start/update functions that might have been passed in
-            startActual = sortable.options.start;
-            updateActual = sortable.options.update;
+            //keep a reference to onDragStart/onDrop functions that might have been passed in
+            onDragStartActual = sortable.options.onDragStart;
+            onDropActual = sortable.options.onDrop;
 
-            //initialize sortable binding after template binding has rendered in update function
+            //initialize sortable binding after template binding has rendered in onDrop function
             var createTimeout = setTimeout(function() {
                 var dragItem;
                 $element.sortable(ko.utils.extend(sortable.options, {
-                    start: function(event, ui) {
+                    onDragStart: function onDragStart($item) {
                         //track original index
-                        var el = ui.item[0];
-                        dataSet(el, INDEXKEY, ko.utils.arrayIndexOf(ui.item.parent().children(), el));
+                        var el = $item[0];
+                        dataSet(el, INDEXKEY, ko.utils.arrayIndexOf($item.parent().children(), el));
 
                         //make sure that fields have a chance to update model
-                        ui.item.find("input:focus").change();
-                        if (startActual) {
-                            startActual.apply(this, arguments);
+                        $item.find("input:focus").change();
+                        if (onDragStartActual) {
+                            onDragStartActual.apply(this, arguments);
                         }
                     },
-                    receive: function(event, ui) {
-                        dragItem = dataGet(ui.item[0], DRAGKEY);
-                        if (dragItem) {
-                            //copy the model item, if a clone option is provided
-                            if (dragItem.clone) {
-                                dragItem = dragItem.clone();
-                            }
-
-                            //configure a handler to potentially manipulate item before drop
-                            if (sortable.dragged) {
-                                dragItem = sortable.dragged.call(this, dragItem, event, ui) || dragItem;
-                            }
-                        }
-                    },
-                    update: function(event, ui) {
+                    onDrop: function onDrop($item, container, _super, event) {
                         var sourceParent, targetParent, sourceIndex, targetIndex, arg,
-                            el = ui.item[0],
-                            parentEl = ui.item.parent()[0],
+                            el = $item[0],
+                            parentEl = $item.parent()[0],
                             item = dataGet(el, ITEMKEY) || dragItem;
 
                         dragItem = null;
 
-                        //make sure that moves only run once, as update fires on multiple containers
-                        if (item && (this === parentEl) || (!hasNestedSortableFix && $.contains(this, parentEl))) {
+                        //make sure that moves only run once, as onDrop fires on multiple containers
+                        if (item && (this === parentEl)) {
                             //identify parents
                             sourceParent = dataGet(el, PARENTKEY);
                             sourceIndex = dataGet(el, INDEXKEY);
                             targetParent = dataGet(el.parentNode, LISTKEY);
-                            targetIndex = ko.utils.arrayIndexOf(ui.item.parent().children(), el);
+                            targetIndex = ko.utils.arrayIndexOf($item.parent().children(), el);
 
                             //take destroyed items into consideration
                             if (!templateOptions.includeDestroyed) {
@@ -209,7 +152,7 @@
                                 arg = {
                                     item: item,
                                     sourceParent: sourceParent,
-                                    sourceParentNode: sourceParent && ui.sender || el.parentNode,
+                                    sourceParentNode: el.parentNode,
                                     sourceIndex: sourceIndex,
                                     targetParent: targetParent,
                                     targetIndex: targetIndex,
@@ -218,18 +161,11 @@
 
                                 //execute the configured callback prior to actually moving items
                                 if (sortable.beforeMove) {
-                                    sortable.beforeMove.call(this, arg, event, ui);
+                                    sortable.beforeMove.call(this, arg, event, $item, container);
                                 }
                             }
 
-                            //call cancel on the correct list, so KO can take care of DOM manipulation
-                            if (sourceParent) {
-                                $(sourceParent === targetParent ? this : ui.sender || this).sortable("cancel");
-                            }
-                            //for a draggable item just remove the element
-                            else {
-                                $(el).remove();
-                            }
+                            _super.apply(this, arguments);
 
                             //if beforeMove told us to cancel, then we are done
                             if (arg && arg.cancelDrop) {
@@ -260,15 +196,14 @@
 
                             //allow binding to accept a function to execute after moving the item
                             if (sortable.afterMove) {
-                                sortable.afterMove.call(this, arg, event, ui);
+                                sortable.afterMove.call(this, arg, event, $item, container);
                             }
                         }
 
-                        if (updateActual) {
-                            updateActual.apply(this, arguments);
+                        if (onDropActual) {
+                            onDropActual.apply(this, arguments);
                         }
-                    },
-                    connectWith: sortable.connectClass ? "." + sortable.connectClass : false
+                    }
                 }));
 
                 //handle enabling/disabling sorting
@@ -289,8 +224,6 @@
                     $element.sortable("destroy");
                 }
 
-                ko.utils.toggleDomNodeCssClass(element, sortable.connectClass, false);
-
                 //do not create the sortable if the element has been removed from DOM
                 clearTimeout(createTimeout);
             });
@@ -306,62 +239,14 @@
             //call template binding's update with correct options
             ko.bindingHandlers.template.update(element, function() { return templateOptions; }, allBindingsAccessor, data, context);
         },
-        connectClass: 'ko_container',
         allowDrop: true,
         afterMove: null,
         beforeMove: null,
-        options: {}
+        options: {},
+        templateEngine: ko.nativeTemplateEngine.instance
     };
 
-    //create a draggable that is appropriate for dropping into a sortable
-    ko.bindingHandlers.draggable = {
-        init: function(element, valueAccessor, allBindingsAccessor, data, context) {
-            var value = unwrap(valueAccessor()) || {},
-                options = value.options || {},
-                draggableOptions = ko.utils.extend({}, ko.bindingHandlers.draggable.options),
-                templateOptions = prepareTemplateOptions(valueAccessor, "data"),
-                connectClass = value.connectClass || ko.bindingHandlers.draggable.connectClass,
-                isEnabled = value.isEnabled !== undefined ? value.isEnabled : ko.bindingHandlers.draggable.isEnabled;
+    ko.expressionRewriting.bindingRewriteValidators['sortable'] = false; // Can't rewrite control flow bindings
+    ko.virtualElements.allowedBindings['sortable'] = true;
 
-            value = "data" in value ? value.data : value;
-
-            //set meta-data
-            dataSet(element, DRAGKEY, value);
-
-            //override global options with override options passed in
-            ko.utils.extend(draggableOptions, options);
-
-            //setup connection to a sortable
-            draggableOptions.connectToSortable = connectClass ? "." + connectClass : false;
-
-            //initialize draggable
-            $(element).draggable(draggableOptions);
-
-            //handle enabling/disabling sorting
-            if (isEnabled !== undefined) {
-                ko.computed({
-                    read: function() {
-                        $(element).draggable(unwrap(isEnabled) ? "enable" : "disable");
-                    },
-                    disposeWhenNodeIsRemoved: element
-                });
-            }
-
-            //handle disposal
-            ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
-                $(element).draggable("destroy");
-            });
-
-            return ko.bindingHandlers.template.init(element, function() { return templateOptions; }, allBindingsAccessor, data, context);
-        },
-        update: function(element, valueAccessor, allBindingsAccessor, data, context) {
-            var templateOptions = prepareTemplateOptions(valueAccessor, "data");
-
-            return ko.bindingHandlers.template.update(element, function() { return templateOptions; }, allBindingsAccessor, data, context);
-        },
-        connectClass: ko.bindingHandlers.sortable.connectClass,
-        options: {
-            helper: "clone"
-        }
-    };
 });
